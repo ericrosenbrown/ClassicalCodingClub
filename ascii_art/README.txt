73b391de3369210f02621520f22787bd7db13a28
ericrosenbrown <eric_rosen@brown.edu>
2016-02-07 17:59:41 -0500

    first

---
+++ ascii_art/README.txt
@@ -0,0 +1,8 @@
+This is a ASCII art generator that takes in an image and then outputs an ASCII representation. 
+
+REQUIREMENTS:
+1) Python
+2) PIL - PIL is an imaging library for Python, you can download it for free from http://www.pythonware.com/products/pil/ 
+
+HOW TO USE:
+1) Download the .zip from github and open up bitMapper.py inside of pixel art. Once you have this file, run the main method by giving it the name of your desired image, and it will output the art to a .txt file in the same folder.
---
+++ ascii_art/bitMapper.py
@@ -0,0 +1,76 @@
+import os, sys
+from PIL import Image
+
+
+"""
+The main function to turn images into pixel art.
+This function takes in a string, which is the filepath to the image
+"""
+def main(fileImage):
+    finalString = "" #This string will hold the final ascii art
+
+    #These next two lines are for loading the picture into python
+    im = Image.open(fileImage).convert("L")
+    pix = im.load()
+
+    #Creates an empty list to hold the ascii strings
+    pictures = []
+
+    
+    #These two for loops loop through the pixels in the image from top-down left-right
+    for x in range(im.size[1]):
+        for y in range(im.size[0]):
+            pictures.append(wFill(pix[y,x])) #Assign an ascii value to each pixel
+        pictures.append("\n") #Add blankspace in string to add new row of ascii characters
+
+    f = open("obama.txt",'wb') #Open the .txt file to write to
+    for x in range(len(pictures)): #For each of the rows in the ascii art, write them to file
+        f.write(pictures[x])
+        finalString += pictures[x]
+    
+def shading(value):
+    if value == 0:
+        return "B"
+    elif (value > 0) and (value <=25):
+        return "W"
+    elif (value > 25) and (value <=50):
+        return "X"
+    elif (value > 50) and (value <=75):
+        return "D"
+    elif (value > 75) and (value <=100):
+        return "A"
+    elif (value > 100) and (value <=125):
+        return "E"
+    elif (value > 125) and (value <=150):
+        return "O"
+    elif (value > 150) and (value <=175):
+        return "["
+    elif (value > 175) and (value <=200):
+        return "}"
+    elif (value > 200) and (value <=225):
+        return "~"
+    elif (value > 225) and (value <=250):
+        return "*"
+    else:
+        return " "
+
+def wFill(v):
+    if v > 120:
+        return " "
+    else:
+        return "W"
+
+def blackAndWhite(v):
+    if v == 255:
+        return "W"
+    elif v == 0:
+        return "B"
+    else:
+        return "G"
+
+def customValueFunction(v):
+    #Put in your own code here and replace the return function
+    return 1
+
+        
+    
