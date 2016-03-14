59b69d034bc3e47d2ed12d39d8c6e58165f757de
ericrosenbrown <eric_rosen@brown.edu>
2016-03-14 14:01:03 -0400

    turtle

---
+++ turtleDrawing/README.txt
@@ -0,0 +1,7 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf130
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 turtle is a python library that allows you to draw using a pen called turtle. You can imagine the turtle like a pen on a piece of paper. Wherever the turtle goes, a line may be drawn below him. Using commands like move forward and turning, you can make complex art. A more in-depth tutorial, that is easy to follow, can be found here (This is where the code examples in our project come from): {\field{\*\fldinst{HYPERLINK "https://michael0x2a.com/blog/turtle-examples"}}{\fldrslt https://michael0x2a.com/blog/turtle-examples}}}
---
+++ turtleDrawing/turtleDrawing.py
@@ -0,0 +1,79 @@
+import turtle
+
+def spiral():
+   wn = turtle.Screen()
+   wn.bgcolor("lightgreen")
+   tess = turtle.Turtle()
+   tess.shape("turtle")
+   tess.color("blue")
+
+   tess.penup()                # This is new
+   size = 20
+   for i in range(30):
+      tess.stamp()             # Leave an impression on the canvas
+      size = size + 3          # Increase the size on every iteration
+      tess.forward(size)       # Move tess along
+      tess.right(24)           #  ...  and turn her
+
+def square():
+   silly = turtle.Turtle()
+
+   silly.forward(50)
+   silly.right(90)     # Rotate clockwise by 90 degrees
+
+   silly.forward(50)
+   silly.right(90)
+
+   silly.forward(50)
+   silly.right(90)
+
+   silly.forward(50)
+   silly.right(90)
+
+   turtle.done()
+
+def spiralStar():
+   spiral = turtle.Turtle()
+
+   for i in range(20):
+       spiral.forward(i * 10)
+       spiral.right(144)
+    
+   turtle.done()
+
+def colorCircles():
+   painter = turtle.Turtle()
+
+   painter.pencolor("blue")
+
+   for i in range(50):
+       painter.forward(50)
+       painter.left(123) # Let's go counterclockwise this time 
+       
+   painter.pencolor("red")
+   for i in range(50):
+       painter.forward(100)
+       painter.left(123)
+       
+   turtle.done()
+
+def lineCircle():
+   ninja = turtle.Turtle()
+
+   ninja.speed(10)
+
+   for i in range(180):
+       ninja.forward(100)
+       ninja.right(30)
+       ninja.forward(20)
+       ninja.left(60)
+       ninja.forward(50)
+       ninja.right(30)
+       
+       ninja.penup()
+       ninja.setposition(0, 0)
+       ninja.pendown()
+       
+       ninja.right(2)
+       
+   turtle.done()
